f49886142501713b63f64d1c9e55f3c95e99c8be
Serpentine-uk <19262752@brookes.ac.uk>
2024-03-04 12:36:33 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Cars Decision Tree Coursework - COMP4035 Computer Science Applications - Artificial Intelligence
+
+# Import necessary modules
+import math
+import pandas as pd
+
+def calculate_entropy():
+    print()
+
+
+if __name__ == "__main__":
+    cardata = pd.read_csv('car.csv')
+    print(cardata)
+
+    # Count the classes
+
+    # Calculate total overall entropy
